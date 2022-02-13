4cb584ad4f32668134045217a87e599c8f0ccc58
Semir Smajlovic <semir.smajlovic@fleishman.com>
2022-02-12 20:05:25 -0600

    Add our sleep function call above fprintf

--- simplechain.c
+++ simplechain.c
@@ -23,6 +23,7 @@ int main(int argc, char * argv[]) {
     if (childpid = fork())
       break;
 
+  sleep(15);
   fprintf(stderr, "i:%d process ID:%ld parent ID:%ld child ID:%ld\n",
     i, (long) getpid(), (long) getppid(), (long) childpid);
 
